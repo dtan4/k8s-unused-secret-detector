a1d1caa6c4255e1011503aee99319ffbabfba467
Daisuke Fujita <dtanshi45@gmail.com>
2019-10-03 13:12:07 +0900

    Print Secrets in default namespace

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/dtan4/k8s-unused-secret-detector
 
 go 1.13
+
+require (
+	github.com/spf13/pflag v1.0.1
+	k8s.io/apimachinery v0.0.0-20190612205821-1799e75a0719
+	k8s.io/client-go v0.0.0-20190620085101-78d2af792bab
+)
--- main.go
+++ main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	fmt.Println("k8s-unused-secret-detector")
+	kubeconfig := clientcmd.RecommendedHomeFile
+	// context := ""
+	namespace := "default"
+
+	clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfig},
+		// &clientcmd.ConfigOverrides{CurrentContext: context},
+		&clientcmd.ConfigOverrides{},
+	)
+
+	config, err := clientConfig.ClientConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	secrets, err := client.CoreV1().Secrets(namespace).List(metav1.ListOptions{})
+	if errors.IsNotFound(err) {
+		panic("secrets not found")
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, secret := range secrets.Items {
+		fmt.Printf("%s\t%s\t%s\n", secret.Namespace, secret.Name, secret.Type)
+	}
 }
