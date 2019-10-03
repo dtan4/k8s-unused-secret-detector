4c483805655ad9ad0c63dc42cba9696caa4d9588
Daisuke Fujita <dtanshi45@gmail.com>
2019-10-03 16:06:00 +0900

    Enable pagination

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/spf13/pflag v1.0.1
+	k8s.io/api v0.0.0-20190620084959-7cf5895f2711
 	k8s.io/apimachinery v0.0.0-20190612205821-1799e75a0719
 	k8s.io/client-go v0.0.0-20190620085101-78d2af792bab
 )
--- main.go
+++ main.go
@@ -1,32 +1,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	flag "github.com/spf13/pflag"
-	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/pager"
 )
 
 func main() {
-	var context, namespace string
+	var kubecontext, namespace string
 
-	flag.StringVar(&context, "context", "", "kubeconfig context")
+	flag.StringVar(&kubecontext, "context", "", "kubeconfig context")
 	flag.StringVarP(&namespace, "namespace", "n", "default", "namespace")
 
 	flag.Parse()
 
-	fmt.Printf("context:   %s\n", context)
+	fmt.Printf("context:   %s\n", kubecontext)
 	fmt.Printf("namespace: %s\n", namespace)
 
 	kubeconfig := clientcmd.RecommendedHomeFile
 
 	clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
 		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfig},
-		&clientcmd.ConfigOverrides{CurrentContext: context},
+		&clientcmd.ConfigOverrides{CurrentContext: kubecontext},
 	)
 
 	config, err := clientConfig.ClientConfig()
@@ -39,16 +42,35 @@ func main() {
 		panic(err)
 	}
 
-	secrets, err := client.CoreV1().Secrets(namespace).List(metav1.ListOptions{})
-	if errors.IsNotFound(err) {
-		panic("secrets not found")
-	}
+	p := pager.New(pager.SimplePageFunc(func(opts metav1.ListOptions) (runtime.Object, error) {
+		list, err := client.CoreV1().Secrets(namespace).List(opts)
+		if err != nil {
+			return nil, err
+		}
 
+		return list, err
+	}))
+	p.PageSize = 500
+
+	ctx := context.Background()
+
+	secrets := []*v1.Secret{}
+
+	err = p.EachListItem(ctx, metav1.ListOptions{}, func(obj runtime.Object) error {
+		secret, ok := obj.(*v1.Secret)
+		if !ok {
+			return err
+		}
+
+		secrets = append(secrets, secret)
+
+		return nil
+	})
 	if err != nil {
 		panic(err)
 	}
 
-	for _, secret := range secrets.Items {
+	for _, secret := range secrets {
 		fmt.Printf("%s\t%s\t%s\n", secret.Namespace, secret.Name, secret.Type)
 	}
 }
